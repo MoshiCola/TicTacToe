3120d539edefeaeed1246f62d919d1978514e163
MoshiCola <160193393+MoshiCola@users.noreply.github.com>
2024-03-11 19:40:00 -0500

    moved inputs into two different functions

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-gameboard = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-x_moves = 0
-o_moves = 0
-
-game_over = False
-
-    
-
 def win_cases(board):
     if board[0] == board[1] == board[2]:
         return True
@@ -34,42 +26,26 @@ def win_cases(board):
         return False
     
 
-def whos_turn_is_it_anyway(last_thing_added_to_board):
-    
-
-
+def whos_turn_is_it_anyway(current_player):
+    if current_player == 'X':
+        return 'O'
+    else:
+        return 'X'  
 
 def moves(player, current_board):
     player_input = input(f'What spot chuwant {player}?:   ')
     spot = int(player_input)
-    if current_board[spot] == spot:
-        current_board[spot] = player
-    print(current_board)
-    return game_over == win_cases(current_board)
+    while current_board[spot] == spot:
+            current_board[spot] = player
+            print(current_board)
+    game_over = win_cases(current_board)
+    return current_board, game_over
 
 
+gameboard = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+player = 'X'
+game_over = False
 
 while game_over == False:
-
-
-
-    x_moves = input(f'What spot chuwant X?:   ')
-    x_spot = int(x_moves)
-    if gameboard[x_spot] == x_spot:
-        gameboard[x_spot] = 'X'
-    print(gameboard)
-    game_over = win_cases(gameboard)
-
-    o_moves = input(f'What spot chuwant O?:   ')
-    o_spot = int(o_moves)
-    if gameboard[o_spot] == o_spot:
-        gameboard[o_spot] = 'O'
-    print(gameboard)
-    game_over = win_cases(gameboard)
-
-    
-
-
-        
-
-
+    gameboard, game_over = moves(player, gameboard)
+    player = whos_turn_is_it_anyway(player)
