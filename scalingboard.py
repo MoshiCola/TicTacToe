62a4f935fd2348dccb03fbc05e4c9a6272119b07
MoshiCola <160193393+MoshiCola@users.noreply.github.com>
2024-04-25 20:05:23 -0500

    began implementaion of a scalable board

---
+++ scalingboard.py
@@ -0,0 +1,11 @@
+def scalingboard(board_size):
+    fullboard = [board_size ** 2]
+    return fullboard
+
+def print_scalingboard(fullboard, board_size):
+    for row in len(board_size):
+        
+
+
+scalingboard(3)
+print_scalingboard(scalingboard(3), 3)
