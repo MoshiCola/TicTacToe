d03c9fa8b4eb32eb9d50576448336aea9fe31484
MoshiCola <160193393+MoshiCola@users.noreply.github.com>
2024-04-25 21:38:09 -0500

    fixed part of scalable board

--- scalingboard.py
+++ scalingboard.py
@@ -1,11 +1,11 @@
 def scalingboard(board_size):
-    fullboard = [board_size ** 2]
-    return fullboard
+    fullboard = [i for i in range(0, board_size ** 2)]
+    print(fullboard)
 
 def print_scalingboard(fullboard, board_size):
-    for row in len(board_size):
-        
+    for row in range(0, board_size):
+        print(row)
 
 
 scalingboard(3)
-print_scalingboard(scalingboard(3), 3)
+print_scalingboard(9, 3)
